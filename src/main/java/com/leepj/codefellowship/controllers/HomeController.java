78610a3b9fb1e655f4f8fb0f219484e737c6cb7c
Peter Lee <leepj85@gmail.com>
2019-08-27 12:24:54 -0700

    initialize

---
+++ src/main/java/com/leepj/codefellowship/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.leepj.codefellowship.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getRoot() {
+        return "root";
+    }
+
+    @GetMapping("/signup")
+    public String getSignup() {
+        return "registration";
+    }
+}
